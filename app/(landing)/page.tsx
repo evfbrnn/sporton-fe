a9aa8695e6fb86110078cfe168ebd26b25a0b815
Your Name <your.email@example.com>
2026-02-01 12:22:37 +0700

    tugas 1

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,11 @@
+import HeroSection from "./componens/home/hero";
+import CategoriesSection from "./componens/home/categories";
+import ProductsSection from "./componens/home/products";
+
+export default function Home() {
+  return <main>
+    <HeroSection/>
+    <CategoriesSection/>
+    <ProductsSection/>
+  </main>;
+}
